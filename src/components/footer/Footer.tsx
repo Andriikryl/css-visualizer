97bc24ecca60d12b8d6e4ab64d0219490d97e758
Andriikryl <andreyk557@gmail.com>
2023-09-13 19:12:32 +0300

    add header / footer / container component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "../container/Container";
+import style from "./style.module.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>Footer</Container>
+    </footer>
+  );
+}
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./style.module.css";
+import { Container } from "../container/Container";
+
+export default function Header() {
+  return (
+    <header>
+      <Container>header</Container>
+    </header>
+  );
+}
