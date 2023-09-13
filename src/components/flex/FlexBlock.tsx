7e7748c35f313b8c3a2227cbce94b234311ff308
Andriikryl <andreyk557@gmail.com>
2023-09-13 23:18:22 +0300

    add new flex component

--- src/components/flex/FlexBlock.tsx
+++ src/components/flex/FlexBlock.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Container } from "../container/Container";
 import style from "./style.module.css";
 import JustyfContent from "./justyfContent/JustyfContent";
+import SpacePos from "./spacePos/SpacePos";
 export default function FlexBlock() {
   return (
     <section className={style.flex__section}>
       <Container>
-        <JustyfContent />
+        <div className={style.flex__group}>
+          <JustyfContent />
+          <SpacePos />
+        </div>
       </Container>
     </section>
   );
