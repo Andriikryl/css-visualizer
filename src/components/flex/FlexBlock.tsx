f12388bf79ec7581d43f36479e624d5f5c3d8139
Andriikryl <andreyk557@gmail.com>
2023-09-14 15:46:04 +0300

    add alieItems component

--- src/components/flex/FlexBlock.tsx
+++ src/components/flex/FlexBlock.tsx
@@ -6,6 +6,7 @@ import SpacePos from "./spacePos/SpacePos";
 import FlexDiraction from "./flexDirection/FlexDiraction";
 import CenterDiv from "./centerDiv/CenterDiv";
 import GridDivCenter from "../grid/gridCenter/GridCenter";
+import AlieItems from "./alieItems/AlieItems";
 export default function FlexBlock() {
   return (
     <section className={style.flex__section}>
@@ -13,6 +14,7 @@ export default function FlexBlock() {
         <div className={style.flex__group}>
           <JustyfContent />
           <SpacePos />
+          <AlieItems />
           <FlexDiraction />
           <CenterDiv />
           <GridDivCenter />
