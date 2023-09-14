74fbc6dea507c0183003395dcf7680d3d8d64592
Andriikryl <andreyk557@gmail.com>
2023-09-14 16:32:56 +0300

    add header nav component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import style from "./style.module.css";
 import { Container } from "../container/Container";
+import Link from "next/link";
+
+const headerLinks = [
+  {
+    id: 1,
+    title: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "Blend-mode",
+    href: "/blend-mod",
+  },
+];
 
 export default function Header() {
   return (
     <header className={style.header}>
       <Container>
-        <h1>Header</h1>
+        <div className={style.header__box}>
+          <nav className={style.nav}>
+            <ul className={style.nav__list}>
+              {headerLinks.map((link) => {
+                return (
+                  <li className={style.list__item} key={link.id}>
+                    <Link className={style.item__link} href={link.href}>
+                      {link.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
       </Container>
     </header>
   );
