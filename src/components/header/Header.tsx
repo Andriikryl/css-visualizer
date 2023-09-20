aefe0ca49b9120000f071209f7890057664af443
Andriikryl <andreyk557@gmail.com>
2023-09-20 18:21:35 +0300

    add functionality to the header nav component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./style.module.css";
 import { Container } from "../container/Container";
 import { motion } from "framer-motion";
@@ -40,10 +40,31 @@ const headerLinks = [
 ];
 
 export default function Header() {
+  const sideNavRef = useRef<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   function handelClickOpen() {
     setIsOpen((prev) => !prev);
   }
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (sideNavRef.current && !sideNavRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    if (html) html.classList.toggle("dis-scroll", isOpen);
+  }, [isOpen]);
 
   return (
     <header className={style.header}>
@@ -52,29 +73,31 @@ export default function Header() {
           <Link href={"/"} className={style.header__title}>
             header
           </Link>
+          <div ref={sideNavRef}>
+            <motion.nav
+              className={style.nav}
+              initial={{ x: -3000 }}
+              animate={{ x: isOpen ? 0 : "-100%" }}
+              transition={{ duration: 0.5 }}
+            >
+              <motion.ul className={style.nav__list}>
+                {headerLinks.map((link) => {
+                  return (
+                    <motion.li className={style.list__item} key={link.id}>
+                      <Link className={style.item__link} href={link.href}>
+                        {link.title}
+                      </Link>
+                    </motion.li>
+                  );
+                })}
+              </motion.ul>
+            </motion.nav>
+          </div>
           <Button
             text="open"
             className={style.button}
             onClick={handelClickOpen}
           ></Button>
-          <motion.nav
-            className={style.nav}
-            initial={{ x: -3000 }}
-            animate={{ x: isOpen ? 0 : "-100%" }}
-            transition={{ duration: 0.5 }}
-          >
-            <motion.ul className={style.nav__list}>
-              {headerLinks.map((link) => {
-                return (
-                  <motion.li className={style.list__item} key={link.id}>
-                    <Link className={style.item__link} href={link.href}>
-                      {link.title}
-                    </Link>
-                  </motion.li>
-                );
-              })}
-            </motion.ul>
-          </motion.nav>
         </div>
       </Container>
     </header>
